7bb0dd1810a23bcc27d50a5d3322c27c22770266
Abhay Kumar Singh <abhay.ksingh.tech2011@gmail.com>
2025-08-07 18:56:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import ttk
+
+def button_func():
+    print("A button was pressed")
+
+# def hello():
+#     print("Hello")
+
+# create a window
+window = tk.Tk()
+window.title("Window and Widgets")
+window.geometry("800x550")
+
+# ttk label
+label = ttk.Label(master = window , text = "This is a NotePad") 
+label.pack()
+
+# tk text
+text =tk.Text(master = window)
+text.pack()
+
+# entry
+entry = ttk.Entry(master = window)
+entry.pack()
+
+# exercise text label
+text_label = tk.Label(master = window , text = "My Label")
+text_label.pack()
+
+#  exercise hello button
+# button1 = ttk.Button(master = window , text = "Hello?" , command = hello)
+button1 = ttk.Button(master = window , text = "Hello?" , command = lambda:print("Hello Bro"))
+button1.pack()
+# ttk button
+button = ttk.Button(master = window , text = "A Button" , command = button_func)
+button.pack()
+
+# run 
+window.mainloop()
